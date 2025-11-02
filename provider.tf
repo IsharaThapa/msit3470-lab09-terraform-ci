c4d51006b9815cacee5705ffc8c7db938f32f71b
ishara <ithapa@clarku.edu>
2025-11-02 08:48:35 -0500

    CI: offline plan (-backend=false, -refresh=false, metadata disabled)

--- provider.tf
+++ provider.tf
@@ -9,15 +9,15 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region                      = "us-east-1"
 
-  # Disable all credential and metadata checks for CI runs
+  # Hard-disable all AWS checks so CI can run without credentials
   skip_credentials_validation = true
   skip_requesting_account_id  = true
   skip_metadata_api_check     = true
   skip_region_validation      = true
 
-  # Add fake credentials to bypass provider auth
+  # Dummy creds to satisfy the provider schema in CI
   access_key = "dummy"
   secret_key = "dummy"
 }
